ce821ecb662d331f7f7519d9bc6bbc9fbcd7d490
ajaz1821-cms <mcu6095@gmail.com>
2026-04-29 22:01:52 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUa3wmEh87fI4jS7GrMw4F_XFgS_7rL08",
+  authDomain: "pizza-heaven-400ab.firebaseapp.com",
+  projectId: "pizza-heaven-400ab",
+  storageBucket: "pizza-heaven-400ab.firebasestorage.app",
+  messagingSenderId: "308101036711",
+  appId: "1:308101036711:web:700487095d112cfd3df982"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
+export const db = getFirestore(app);
