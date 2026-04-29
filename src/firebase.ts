00c77430520c5319a3d1ee537e169ec661a0310a
ajaz1821-cms <mcu6095@gmail.com>
2026-04-29 22:13:51 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUa3wmEh87fI4jS7GrMw4F_XFgS_7rL08",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app); // Ye line missing thi isliye error aaya
